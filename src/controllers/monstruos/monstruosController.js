942fc37b78a02bba144b256b4089194f5dd09d38
JorgePascualFuentecilla <jorgepasf@gmail.com>
2024-12-10 19:45:15 +0100

    Codigo sin usar borrado

--- src/controllers/monstruos/monstruosController.js
+++ src/controllers/monstruos/monstruosController.js
@@ -1,7 +1,7 @@
 import { Monstruo,Poder } from '../../models/index.js';
 const includes = {include: {
-    model: Poder, // Incluir el modelo Poder
-    through: { attributes: [] }, // Excluir la tabla intermedia
+    model: Poder,
+    through: { attributes: [] }, 
   }}
 async function getAll() {
     const users = await Monstruo.findAll(includes)
@@ -12,46 +12,13 @@ async function getById(id) {
     return user;
 }
 
-/*async function create(username, email, password) {
-    const oldUser = await getByEmail(id);
-    if(oldUser){
-        throw new error.EMAIL_ALREADY_EXISTS;
-    }
-    const newUser = await monstruos.create({
-        username,
-        email,
-        password         
-    });
 
-    return newUser;
-}*/
-
-
-/*async function update(id, username, email, rol,password=null) {
-    const user = await monstruos.findByPk(id);
-
-    user.username = username;
-    user.email = email;
-    user.rol = rol;
-    if (password) {user.password=password}
-   
-    await user.save();
-    return user;
-}*/
-
-/*async function remove(id) {
-    const userToRemove = await monstruos.findByPk(id);
-    await userToRemove.destroy();
-    return userToRemove;
-}*/
 
 
 export const functions = {
     getAll,
     getById,
-    //create,
-    //update,
-    //remove
+    
 }
 
 export default functions;
--- src/controllers/villanos/villanosController.js
+++ src/controllers/villanos/villanosController.js
@@ -10,46 +10,12 @@ async function getById(id) {
     return user;
 }
 
-/*async function create(username, email, password) {
-    const oldUser = await getByEmail(id);
-    if(oldUser){
-        throw new error.EMAIL_ALREADY_EXISTS;
-    }
-    const newUser = await villanos.create({
-        username,
-        email,
-        password         
-    });
 
-    return newUser;
-}*/
-
-
-/*async function update(id, username, email, rol,password=null) {
-    const user = await villanos.findByPk(id);
-
-    user.username = username;
-    user.email = email;
-    user.rol = rol;
-    if (password) {user.password=password}
-   
-    await user.save();
-    return user;
-}*/
-
-/*async function remove(id) {
-    const userToRemove = await villanos.findByPk(id);
-    await userToRemove.destroy();
-    return userToRemove;
-}*/
 
 
 export const functions = {
     getAll,
     getById,
-    //create,
-    //update,
-    //remove
+   
 }
-
 export default functions;
